4d28924479303a1fac14b2637eae721d9db1b35d
pavan <pavan@LAPTOP-OB1CBGQ8>
2021-03-15 12:54:52 +0530

    COMMIT TO GIT LOCAL

---
+++ githubtrial/src/test/java/com/cast/createcontactTest/ContactTest.java
@@ -0,0 +1,22 @@
+package com.cast.createcontactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+
+	@Test
+	public void createcontacttest()
+	{
+		System.out.println("execute create contact test");
+	}
+	@Test
+	public void modifycontacttest()
+	{
+		System.out.println("execute modify contact test");
+	}
+	@Test
+	public void deletecontacttest()
+	{
+		System.out.println("execute delete contact test");
+	}
+}
